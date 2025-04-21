7812fe35bb4105df563c7abb74d938c5f6946204
Toni Moreira <moreiraatoni@gmail.com>
2025-04-21 16:31:29 -0300

    feat: add order updates controller and validation schema

---
+++ app/controllers/order_updates_controller.ts
@@ -0,0 +1,20 @@
+import { Order } from '#models/order';
+import { OrderUpdate } from '#models/order_update';
+import { createOrderUpdateSchema } from '#validators/order_update'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class OrderUpdatesController {
+
+    async store({ request }: HttpContext)
+    {
+        const payload = await request.validateUsing(createOrderUpdateSchema)
+
+        const order = await Order.findOrFail(payload.orderId)
+        order.status = payload.status
+        const orderUpdate = await OrderUpdate.create(payload)
+        await order.save()
+
+        return orderUpdate;
+    }
+
+}
